31e0b714e2c986e051ac6d15dce33eb9472d0944
Christian Walpen <c.walpen@gmx.net>
2016-03-15 21:50:36 +0100

    Added Project

---
+++ src/PersistenceMap.Interception/DataReaderInterceptor.cs
@@ -0,0 +1,27 @@
+﻿using PersistenceMap.QueryBuilder;
+using System.Collections.Generic;
+
+namespace PersistenceMap.Interception
+{
+    internal class DataReaderInterceptor<T> : IInterceptor<T>
+    {
+        private readonly MockedDataReader<T> _dataReader;
+
+        public DataReaderInterceptor(IEnumerable<T> result)
+        {
+            _dataReader = new MockedDataReader<T>(result);
+        }
+
+        public void VisitBeforeExecute(CompiledQuery query, IDatabaseContext context)
+        {
+            var kernel = new MockedQueryKernel(context);
+            kernel.AddDataReader<T>(_dataReader);
+
+            context.Kernel = kernel;
+        }
+        
+        public void VisitBeforeCompile(IQueryPartsContainer container)
+        {
+        }
+    }
+}
---
+++ src/PersistenceMap.Interception/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PersistenceMap.Interception")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("PersistenceMap.Interception")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("0b9135ac-9015-43a6-960b-5d47c25f46e4")]
+
